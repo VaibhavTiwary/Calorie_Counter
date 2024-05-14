395fc443ca3a66ba9cc9ab6f16f439f76dd3a2fd
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-05-14 20:25:10 +0530

    add forms and fetch API

--- counter/views.py
+++ counter/views.py
@@ -4,16 +4,22 @@ from django.shortcuts import render
 
 
 def home(request):
+    import json
     import requests
 
-    query = "1lb brisket and fries"
-    api_url = "https://api.api-ninjas.com/v1/nutrition?query={}".format(query)
-    response = requests.get(
-        api_url, headers={"X-Api-Key": "cyvsj4SiT6khFNVhheDsug==0c6Okza3qcpIODgg"}
-    )
-    if response.status_code == requests.codes.ok:
-        print(response.text)
+    if request.method == "POST":
+        query = request.POST["query"]
+        api_url = "https://api.api-ninjas.com/v1/nutrition?query="
+        api_request = requests.get(
+            api_url + query,
+            headers={"X-Api-Key": "cyvsj4SiT6khFNVhheDsug==0c6Okza3qcpIODgg"},
+        )
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, "home.html", {"api": api})
     else:
-        print("Error:", response.status_code, response.text)
-
-    return render(request, "home.html")
+        return render(request, "home.html", {"query": "Enter a valid query"})
